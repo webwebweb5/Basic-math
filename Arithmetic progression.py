e20330bdae196643e3d150782a8cd06c3b70d76c
webwebweb5 <68845415+webwebweb5@users.noreply.github.com>
2020-09-11 19:17:42 +0700

    Create Arithmetic progression.py

---
+++ Arithmetic progression.py
@@ -0,0 +1,14 @@
+#Arithmetic progression
+import math
+
+print('Arithmetic progression')
+print('use to find Sn')
+print('Sn =(n/2*(2*a1+(n-1)*d)')
+a1 = int(input('a1(the initial term) : '))
+n = int(input('n : '))
+d = int(input('d(the common difference) : '))
+
+def Sequences(n):
+    return int(n/2*(2*a1+(n-1)*d))
+
+print('Sn = ' + str(Sequences(n)))
