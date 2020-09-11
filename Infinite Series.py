95bc8f6bba00fe92f6a3d829c99db0c78fcfd5dc
webwebweb5 <68845415+webwebweb5@users.noreply.github.com>
2020-09-11 19:54:02 +0700

    Create Infinite Series.py

---
+++ Infinite Series.py
@@ -0,0 +1,14 @@
+#Infinite Series
+import math
+
+print('Infinite Series')
+print('use to find S∞')
+print('S∞ = a1/(1/r)')
+
+a1 = float(input('a1(the initial term) : '))
+r = float(input('r : '))
+
+def Sequences():
+    return float(a1/(1-r))
+
+print('Sn = ' + str(Sequences()))
